20e7d9cfb18741e2e1d97de4666043c98359b4db
Gabriel Coelho Soares <soares@workmail.com>
2017-08-08 13:10:32 -0300

    Adicao da documentacao - formato antigo

--- app/views/index.php
+++ app/views/index.php
@@ -5,6 +5,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="./public/css/style.css" rel="stylesheet">
+        <link href="./public/css/font-awesome/font-awesome.css" rel="stylesheet">
+        <link href="./public/css/font-awesome/font-awesome.min.css" rel="stylesheet">
     </head>
     <body>
         <div class="wrapperbody">
@@ -15,12 +17,14 @@
               <div class="logoleft">
                 TechTrain
               </div>
-              <div class="atributosright">
+              <button class="btn-menu bg-gradient"><i class="fa fa-bars fa-lg"></i></button>
+              <div class="menu">
+                  <a href="" class="btn-close">x</a>
                   <ul>
-                      <li>Pesquisar</li>
-                      <li>Cursos</li>
-                      <li>Certificados</li>
-                      <li>Login</li>
+                      <li><a href="">Pesquisar</a></li>
+                      <li><a href="">Cursos</a></li>
+                      <li><a href="">Certificados</a></li>
+                      <li><a href="">Login</a></li>
                   </ul>
               </div>
             </nav>
@@ -28,5 +32,15 @@
             <aside class="lateral"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempora, nemo expedita tempore perspiciatis deserunt ipsam, assumenda, at nisi beatae accusantium! Quas autem dolores, vero repellendus similique ullam aspernatur cumque. </p></aside>
             <footer class="rodape"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempora, nemo expedita tempore perspiciatis deserunt ipsam, assumenda, at nisi beatae accusantium! Quas autem dolores, vero repellendus similique ullam aspernatur cumque. </p></footer>
         </div>
+        <!-- JQUERY --> 
+        <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>
+        $(".btn-menu").click(function(){
+          $(".menu").show();
+        });
+        $(".btn-close").click(function(){
+          $(".menu").hide();
+        });
+        </script>      
     </body>
 </html>
