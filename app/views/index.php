ada8a94c2ed5ae389832787b002d567584c95add
Gabriel Coelho <soares@workmail.com>
2017-09-09 14:03:10 -0300

    Adicao de um template e alteracao

--- app/views/index.php
+++ app/views/index.php
@@ -1,46 +1,444 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title></title>
+        <title>TechTrain</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="./public/css/style.css" rel="stylesheet">
-        <link href="./public/css/font-awesome/font-awesome.css" rel="stylesheet">
-        <link href="./public/css/font-awesome/font-awesome.min.css" rel="stylesheet">
+        <link href="./public/css/bootstrap.min.css" rel="stylesheet">
+        <link href="./public/css/responsive-slider.css" rel="stylesheet">
+        <link rel="stylesheet" href="./public/css/animate.css">
+        <link rel="stylesheet" href="./public/css/font-awesome.min.css">
+        <link href="./public/css/style.css" rel="stylesheet">	
     </head>
     <body>
-        <div class="wrapperbody">
-            <header class="cabecalho">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempora, nemo expedita tempore perspiciatis deserunt ipsam, assumenda, at nisi beatae accusantium! Quas autem dolores, vero repellendus similique ullam aspernatur cumque.
-            </header>
-            <nav class="navegacao">
-              <div class="logoleft">
-                TechTrain
-              </div>
-              <button class="btn-menu bg-gradient"><i class="fa fa-bars fa-lg"></i></button>
-              <div class="menu">
-                  <a href="" class="btn-close">x</a>
-                  <ul>
-                      <li><a href="">Pesquisar</a></li>
-                      <li><a href="">Cursos</a></li>
-                      <li><a href="">Certificados</a></li>
-                      <li><a href="">Login</a></li>
-                  </ul>
-              </div>
-            </nav>
-            <main class="principal"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempora, nemo expedita tempore perspiciatis deserunt ipsam, assumenda, at nisi beatae accusantium! Quas autem dolores, vero repellendus similique ullam aspernatur cumque. </p></main>
-            <aside class="lateral"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempora, nemo expedita tempore perspiciatis deserunt ipsam, assumenda, at nisi beatae accusantium! Quas autem dolores, vero repellendus similique ullam aspernatur cumque. </p></aside>
-            <footer class="rodape"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempora, nemo expedita tempore perspiciatis deserunt ipsam, assumenda, at nisi beatae accusantium! Quas autem dolores, vero repellendus similique ullam aspernatur cumque. </p></footer>
-        </div>
-        <!-- JQUERY --> 
-        <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <script>
-        $(".btn-menu").click(function(){
-          $(".menu").show();
-        });
-        $(".btn-close").click(function(){
-          $(".menu").hide();
-        });
-        </script>      
+    <header>
+		<div class="container">
+			<div class="row">
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<div class="navbar-brand">
+								<a href="index.html"><h1>TechTRAIN</h1></a>
+							</div>
+						</div>
+						<div class="menu">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation" class="active"><a href="index.html">Home</a></li>
+								
+								<li class="blog" role="presentation"><a href="blog.html">Blog</a></li>
+								<li class="curso" role="presentation"><a href="portfolio.html">Cursos</a></li>
+                                <li class="cert" role="presentation"><a href="contacts.html">Certificados</a></li>
+                                <li class="login" role="presentation"><a href="contacts.html">Login</a></li>							
+							</ul>
+						</div>
+					</div>			
+				</nav>
+			</div>
+		</div>
+	</header>
+	
+	<!-- Responsive slider - START -->
+	<div class="slider">
+	<div class="container">
+		<div class="row">
+			<div class="responsive-slider" data-spy="responsive-slider" data-autoplay="true">
+				<div class="slides" data-group="slides">
+					<ul>
+						<li>
+							<div class="slide-body" data-group="slide">
+								<img src="./public/images/2a.jpg" alt="">
+								<div class="caption header" data-animate="slideAppearUpToDown" data-delay="500" data-length="300">
+									<button class="btn btn-primary"><h2>Vídeo aulas ao seu alcance</h2></button>
+									<div class="caption-sub" data-animate="slideAppearDownToUp" data-delay="1200" data-length="300"><button class="btn btn-primary"><h4><span>Reunidas em um só lugar</span></h4></button></div>
+									<div class="caption-sub" data-animate="slideAppearLeftToRight" data-delay="900" data-length="300"><button class="btn btn-primary"><h3>Encontre Facilmente</h3></button></div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="slide-body" data-group="slide">
+								<img src="./public/images/1.jpg" alt="">
+								<div class="caption header" data-animate="slideAppearDownToUp" data-delay="500" data-length="300">
+									<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"><h2>Fotografia, Design e outros</h2></button>
+									<div class="caption-sub" data-animate="slideAppearUpToDown" data-delay="800" data-length="300"><button class="btn btn-primary"><h4><span>Aprenda e classifique os professores</span></h4></button></div>
+									<div class="caption-sub" data-animate="slideAppearRightToLeft" data-delay="1200" data-length="300"><button class="btn btn-primary"><h3>Tudo na palma de sua mão</h3></button></div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="slide-body" data-group="slide">
+								<img src="./public/images/10.jpg" alt="">
+								<div class="caption header" data-animate="slideAppearUpToDown" data-delay="500" data-length="300">
+								  <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"><h2>Foco no aprendizado</h2></button>
+								  <div class="caption-sub" data-animate="slideAppearLeftToRight" data-delay="800" data-length="300"><button class="btn btn-primary"><h4>Cursos pagos: apoio diretamente com o professor</h4></button></div>
+								  <div class="caption-sub" data-animate="slideAppearDownToUp" data-delay="1200" data-length="300"><button class="btn btn-primary"><h3><span>Não perca tempo!</span></h3></button></div>
+						
+								</div>
+							</div>
+						</li>
+				
+					</ul>
+				</div>
+		   
+				<a class="slider-control left" href="#" data-jump="prev"><i class="fa fa-angle-left fa-2x"></i></a>
+				<a class="slider-control right" href="#" data-jump="next"><i class="fa fa-angle-right fa-2x"></i></a>		
+			</div>
+		</div>
+	</div>
+	</div>
+    <!-- Responsive slider - END -->
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="contents">
+					<h2>Junte o poder do <span>YouTube</span> e da <span>qualidade</span>dos professores</h2>
+					<p>Tenha um <em>supersite</em> de aprendizado na palma de sua mão.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	
+	<div class="container">
+		<div class="row">
+			<div class="recent">
+				<button class="btn-primarys"><h3>Cursos disponíveis</h3></button>
+				<hr>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="content">
+				<div class="col-md-4">
+					<div class="wow flipInY" data-wow-offset="0" data-wow-delay="0.4s">
+						<div class="align-center">
+							<h4>PHP Básico</h4>					
+							<div class="icon">
+								<i class="fa fa-code fa-3x"></i>
+							</div>
+							<p>
+							 Aprenda a poderosa linguagem de programação <em>back-end</em>.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Aprenda conosco!</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="wow flipInY" data-wow-offset="0" data-wow-delay="0.8s">
+						<div class="align-center">
+							<h4>Fotografia</h4>					
+							<div class="icon">
+								<i class="fa fa-camera fa-3x"></i>
+
+							</div>
+							<p>
+							 Não é o lugar que deixa a foto bonita, e sim o fotográfo!
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Aprenda conosco!</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="wow flipInY" data-wow-offset="0" data-wow-delay="1.2s">
+						<div class="align-center">
+							<h4>Design Gráfico</h4>					
+							<div class="icon">
+								<i class="fa fa-font fa-3x"></i>
+
+							</div>
+							<p>
+							 Aprenda de uma vez por todas como ser designer gráfico.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Aprenda Conosco!</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<div class="container">
+		<div class="row">
+			<div class="box">
+				
+				<div class="col-md-6">
+					<div class="wow bounceIn" data-wow-offset="0" data-wow-delay="0.8s">
+						<div class="align-center">
+							<h4>Quick Support</h4>					
+							<div class="icon">
+								<i class="fa fa-heart-o fa-3x"></i>
+
+							</div>
+							<p>
+							 Voluptatem accusantium doloremque laudantium sprea totam rem aperiam.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Learn more</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="wow bounceIn" data-wow-offset="0" data-wow-delay="1.3s">
+						<div class="align-center">
+							<h4>Easy Customize</h4>					
+							<div class="icon">
+								<i class="fa fa-laptop fa-3x"></i>
+
+							</div>
+							<p>
+							 Voluptatem accusantium doloremque laudantium sprea totam rem aperiam.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Learn more</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<!--about-->
+	
+		<div class="container">
+			<div class="about">			
+					<div class="row">
+						<div class="recent">
+							<button class="btn-primarys"><h3>Curso Promovido do Mês</h3></button>
+							<hr>
+						</div>
+					</div>				
+				<div class="row">			
+					<div class="row-slider">
+						<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.2s">
+							<div class="col-lg-6 mar-bot30">
+								<div class="responsive-slider" data-spy="responsive-slider" data-autoplay="true">
+									<div class="slides" data-group="slides">
+										<ul> 	    		
+											<div class="slide-bodys" data-group="slide">
+												<li><img alt="" class="img-responsive" src="./public/images/3.jpg" width="100%" height="450"/></li>
+												<li><img alt="" class="img-responsive" src="./public/images/4.jpg" width="100%" height="450"/></li>
+												<li><img alt="" class="img-responsive" src="./public/images/5.jpg" width="100%" height="450"/></li>
+											</div>
+										</ul>
+											
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
+								<div class="thumnails">
+									<h4>IoT + JavaScript e Firebase - Professor Binho</h4>													
+									<p>Nesta série de quatro vídeos, eu vou mostrar como você 
+                                        pode controlar dispositivos elétricos de sua casa a partir 
+                                        da internet. Usando Arduino, Javascript, Node.js, HTML + CSS 
+                                        e Firebase, você verá como é possível criar um site para 
+                                        controlar suas 'COISAS', a partir da Internet.</p>								
+									
+									
+									<div class="ficon">
+										<a href="" alt="">Aprenda Conosco</a> <i class="fa fa-long-arrow-right"></i>
+									</div>
+								</div>
+							</div>
+						</div>					
+					</div>	
+				</div>					
+			</div>			
+		</div>
+	
+	<!--/about-->
+	
+	
+	<div class="container">
+		<div class="row">
+			<div class="recent">
+				<button class="btn-primarys"><h3>Comentário dos Alunos</h3></button>
+				<hr>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="content">
+				<div class="col-md-4">
+					<div class="wow fadeInUp" data-wow-delay="0.2s">
+						<div class="align-center">
+							<h4>Henrique Augusto</h4>	
+							<div class="icon">
+								<i class="fa fa-comments-o fa-3x"></i>
+							</div>
+							<img src="./public/images/team/3.jpg" alt="" class="img-responsive">
+							<p>
+                             Gostei muito do curso, me ajudou bastante a resolver os problemas dos trabalhos que peguei como freelancer. 
+                             Agradeço muito a TechTrain por disponibilizar vários cursos grátis e pagos em um único site.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Veja mais comentários</a><i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="wow fadeInUp" data-wow-delay="0.4s">
+						<div class="align-center">
+							<h4>Lucas Silva e Silva</h4>					
+							<div class="icon">
+								<i class="fa fa-comments-o fa-3x"></i>
+							</div>
+							<img src="./public/images/13.jpg" alt="" class="img-responsive">
+							<p>
+							 A quantidade de instrutores diferentes para o mesmo curso me deixa escolher com qual deles quero aprender.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Veja mais comentários</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="wow fadeInUp" data-wow-delay="0.6s">
+						<div class="align-center">
+							<h4>Kelly Cristine</h4>					
+							<div class="icon">
+								<i class="fa fa-comments-o fa-3x"></i>
+							</div>
+							<img src="./public/images/team/members3.jpg" alt="" class="img-responsive">
+							<p>
+							 Confesso que fiquei receosa no princípio, mas TechTrain com certeza tem um futuro ao lado de Udemy e Udacity.
+							</p>
+							<div class="ficon">
+								<a href="" alt="">Veja mais Comentários</a> <i class="fa fa-long-arrow-right"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<hr>
+		</div>
+	</div>
+	
+	<!--start footer-->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Entre em Contato</h5>
+						<address>
+						<strong>TechTrain Group</strong><br>
+						 Avenida Bandeirantes, 2589 Jd Ype 1<br>
+						 Mogi Guaçu - São Paulo 13846000 Brasil
+						</address>
+						<p>
+							<i class="icon-phone"></i> +55 (19) 3831-0000 - +55 (19) 3831-0022 <br>
+							<i class="icon-envelope-alt"></i> contato@techtraincurso.com.br
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Páginas</h5>
+						<ul class="link-list">
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Termos e Condições</a></li>
+							<li><a href="#">Política de Privacidade</a></li>
+							<li><a href="#">Trabalhe conosco</a></li>
+							<li><a href="#">Entre em Contato</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Últimos Posts</h5>
+						<ul class="link-list">
+							<li><a href="#">Jornada de desenvolvimento</a></li>
+							<li><a href="#">Aprendizado de Github para o Grupo</a></li>
+							<li><a href="#">Pedras no caminho</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Instagram do Grupo</h5>
+						<div class="flickr_badge">
+							<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=34178660@N03"></script>
+						</div>
+						<div class="clear">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container">
+			<div class="row">
+				<hr>
+			</div>
+		</div>
+		
+		<div id="sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="copyright">
+							<p>
+								<span>&copy; TechTrain Group 2017 Todos os Direitos Reservados. Por </span><a href="https://github.com/GabrielCoelho" target="_blank">Gabriel Coelho</a> e <a href="https://github.com/vcbecaleti">Vinicius Becaleti</a>
+							</p>
+                            <!-- 
+                                All links in the footer should remain intact. 
+                                Licenseing information is available at: http://bootstraptaste.com/license/
+                                You can buy this theme without footer links online at: http://bootstraptaste.com/buy/?theme=Arsha
+                            -->
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<ul class="social-network">
+							<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus fa-1x"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--end footer-->
+	
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="./public/js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="./public/js/bootstrap.min.js"></script>
+	<script src="./public/js/responsive-slider.js"></script>
+	<script src="./public/js/wow.min.js"></script>
+	<script>
+	wow = new WOW(
+	 {
+	
+		}	) 
+		.init();
+	</script>
     </body>
 </html>
