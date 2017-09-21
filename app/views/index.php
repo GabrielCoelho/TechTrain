e75897b64a3433685174a47c4333311f92cc5bd1
Gabriel Coelho <soares@workmail.com>
2017-09-21 00:55:35 -0300

    hotfix

--- app/views/index.php
+++ app/views/index.php
@@ -421,7 +421,24 @@
 					<div class="widget">
 						<h5 class="widgetheading">Instagram do Grupo</h5>
 						<div class="flickr_badge">
-							<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=34178660@N03"></script>
+						<blockquote class="instagram-media" data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+							<div style="padding:8px;"> 
+								<div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;"> 
+									<div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;">
+									</div>
+								</div> 
+								<p style=" margin:8px 0 0 0; padding:0 4px;"> 
+									<a href="https://www.instagram.com/p/BZPZuhIFrLE/" style=" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank">
+									Our team specialized in database modeling. They are: Richard Dias and Luis Fernando. They&#39;re working now in SQL of our Database!  #programmer #git #github #web #webdesign #SQL #MySQL #MariaDB #database #development #webdevelopment #webdev #code #codero
+									</a>
+								</p> 
+								<p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">
+								Uma publicação compartilhada por TechTrain Group (@techtraingroup) em 
+								<time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2017-09-19T23:14:31+00:00">Set 19, 2017 às 4:14 PDT</time>
+								</p>
+							</div>
+						</blockquote>
+							<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
 						</div>
 						<div class="clear">
 						</div>
