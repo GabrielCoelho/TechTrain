f2da0f41ee5308b7486513b1674b87a2eabd0304
Gabriel Coelho Soares <soares@workmail.com>
2017-10-04 12:43:44 -0300

    Commented all the testing functions of Course Controller. Have to restart all the development because of the new Database

--- app/Controller/CourseController.php
+++ app/Controller/CourseController.php
@@ -9,26 +9,26 @@ class CourseController
   public function searchCourses()
   {
     $q = new QueryBuilder();
-    $array = $q->select("cursos",["free" => 0]);
+    $array = $q->select("curso",["free" => 0]);
     
-    $curso['info'] = array_filter($array, function($x){
-      return $x['poloEducacional'] == "Informática";
-    });
-    $curso['adm'] = array_filter($array, function($x){
-      return $x['poloEducacional'] == "Administração";
-    });
-    $curso['foto'] = array_filter($array, function($x){
-      return $x['poloEducacional'] == "Fotografia";
-    });
-    $curso['musica'] = array_filter($array, function($x){
-      return $x['poloEducacional'] == "Música";
-    });
-    $curso['design'] = array_filter($array, function($x){
-      return $x['poloEducacional'] == "Design";
-    });
+    // $curso['info'] = array_filter($array, function($x){
+    //   return $x['poloEducacional'] == "Informática";
+    // });
+    // $curso['adm'] = array_filter($array, function($x){
+    //   return $x['poloEducacional'] == "Administração";
+    // });
+    // $curso['foto'] = array_filter($array, function($x){
+    //   return $x['poloEducacional'] == "Fotografia";
+    // });
+    // $curso['musica'] = array_filter($array, function($x){
+    //   return $x['poloEducacional'] == "Música";
+    // });
+    // $curso['design'] = array_filter($array, function($x){
+    //   return $x['poloEducacional'] == "Design";
+    // });
 
-    // print_r($curso);
-    // die();
+    // // print_r($curso);
+    // // die();
 
     require './app/views/cursos.php';
   }
