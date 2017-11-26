7108c87cbd43b7188e05187a15bbb9dd1d74468f
lfrclnando <ferlui7817@outlook.com>
2017-11-26 03:31:19 -0200

    Banco de Dados novo e Documentação melhorada (TCC + Análise de Sistemas.

--- Docs/About DB/Modeling/FISICO_TechTRAIN.sql
+++ Docs/About DB/Modeling/FISICO_TechTRAIN.sql
@@ -3,65 +3,72 @@
 
 
 
-CREATE TABLE Grade_Curso (
-idGrade Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-nmGrade Varchar(60) NOT NULL, 
-PRIMARY KEY (idGrade)
-);
-
-CREATE TABLE Vídeo (
-idVideo Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-idCurso Integer(11) NOT NULL, 
-ordNume_Video Integer(11) DEFAULT NULL,
-urlVideo Varchar(50) DEFAULT NULL,
-PRIMARY KEY (idVideo)
-);
-
-CREATE TABLE Usuario (
-idUsuario Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-nmUsuario Varchar(60) NOT NULL,
-senhaUsuario Varchar(20) NOT NULL,
-emailUsuario Varchar(30) NOT NULL,
-PRIMARY KEY (idUsuario)
+CREATE TABLE Categorias (
+idCategoria Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
+nmCategoria Varchar(60) NOT NULL UNIQUE,
+PRIMARY KEY (idCategoria)
 );
 
 CREATE TABLE Curso (
 idCurso Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-idGrade Integer(11) NOT NULL UNIQUE,
-nmCurso Varchar(60) NOT NULL,
+idCategoria Integer(11) NOT NULL UNIQUE,
+nmCurso Varchar(60) NOT NULL UNIQUE,
 infoCurso_Simples Varchar(20) NOT NULL,
 infoCurso_Extensa LongText DEFAULT NULL,
 dataCurso Date NOT NULL,
-imagCurso Varchar(255) NOT NULL,
-PRIMARY kEY (idCurso),
-FOREIGN KEY(idGrade) REFERENCES Grade_Curso (idGrade)
-);
-
-CREATE TABLE Professor (
-idProfessor Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-nmProfessor Varchar(60) NOT NULL,
-canalProfessor Varchar(255),
-PRIMARY KEY (idProfessor)
-);
-
-CREATE TABLE Ministra_Curso (
-idMiniCurso Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-idProfessor Integer(11) NOT NULL,
-idCurso Integer(11) NOT NULL,
-cargaHorariaCurso Time NOT NULL,
-PRIMARY KEY (idMiniCurso),
-FOREIGN KEY(idProfessor) REFERENCES Professor (idProfessor),
-FOREIGN KEY(idCurso) REFERENCES Curso (idCurso)
+imagCurso Varchar(255) DEFAULT NULL,
+CONSTRAINT PK_CURSO PRIMARY KEY (idCurso),
+FOREIGN KEY (idCategoria) REFERENCES Categorias (idCategoria)
 );
 
 CREATE TABLE Faz_Curso (
 idFazCurso Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
-idCurso Integer(11) NOT NULL,
-idUsuario Integer(11) NOT NULL,
-dataInscrição Date NOT NULL,
+idUsuario Integer(11) NOT NULL UNIQUE,
+idCurso Integer(11) NOT NULL UNIQUE,
+dataInscricao Date NOT NULL UNIQUE,
 PRIMARY KEY (idFazCurso),
-FOREIGN KEY(idCurso) REFERENCES Curso (idCurso),
-FOREIGN KEY(idUsuario) REFERENCES Usuario (idUsuario)
+CONSTRAINT FK_UsuarioFaz_Curso FOREIGN KEY (idUsuario)
+REFERENCES Usuario (idUsuario)
 );
 
-ALTER TABLE Vídeo ADD FOREIGN KEY(idCurso) REFERENCES Curso (idCurso)
+CREATE TABLE Usuario (
+idUsuario Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
+nmUsuario Varchar(60) NOT NULL UNIQUE,
+senhaUsuario Varchar(20) NOT NULL UNIQUE,
+emailUsuario Varchar(30) NOT NULL UNIQUE,
+PRIMARY KEY (idUsuario)
+);
+
+CREATE TABLE Professor (
+idProfessor Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
+nmProfessor Varchar(60) NOT NULL UNIQUE,
+PRIMARY KEY (idProfessor)
+);
+
+CREATE TABLE canalProfessor (
+canalProfessor Varchar(255) DEFAULT NULL
+);
+
+CREATE TABLE Ministra_Curso (
+idMiniCurso Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
+idProfessor Integer(11) NOT NULL UNIQUE,
+idCurso Integer(11) NOT NULL UNIQUE,
+cargaHorariaCurso Time NOT NULL UNIQUE,
+PRIMARY KEY (idMiniCurso),
+CONSTRAINT FK_ProfessorMinistra_Curso FOREIGN KEY (idProfessor)
+REFERENCES Professor (idProfessor),
+CONSTRAINT FK_CursoMinistra_Curso FOREIGN KEY (idCurso)
+REFERENCES Curso (idCurso)
+);
+
+CREATE TABLE Video (
+idVideo Integer(11) NOT NULL AUTO_INCREMENT UNIQUE,
+idCurso Integer(11) NOT NULL,
+ordNume_Video Integer(11) NOT NULL,
+urlVideo Varchar(255) NOT NULL,
+PRIMARY KEY (idVideo),
+CONSTRAINT FK_CursoVideo FOREIGN KEY (idCurso)
+REFERENCES Curso (idCurso)
+);
+
+ALTER TABLE Faz_Curso ADD FOREIGN KEY(idUsuario) REFERENCES Usuario (idUsuario)
